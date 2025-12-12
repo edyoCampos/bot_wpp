814a19b7f01b6999f31d6ddb643c7925601d29e1
edyoCampos <camposevero@gmail.com>
2025-12-12 18:58:28 -0300

    refactor: refatora api.py

--- src/robbot/api/v1/routers/api.py
+++ src/robbot/api/v1/routers/api.py
@@ -6,6 +6,7 @@ from robbot.adapters.controllers import (
     auth_controller,
     health_controller,
     message_controller,
+    queue_controller,
     user_controller,
     waha_controller,
     webhook_controller,
@@ -18,6 +19,8 @@ api_router.include_router(health_controller.router, prefix="", tags=["health"])
 api_router.include_router(user_controller.router, prefix="", tags=["users"])
 api_router.include_router(message_controller.router,
                           prefix="/messages", tags=["messages"])
+api_router.include_router(queue_controller.router,
+                          prefix="", tags=["queue"])
 api_router.include_router(waha_controller.router, prefix="")
 api_router.include_router(webhook_controller.router,
                           prefix="", tags=["Webhooks"])
