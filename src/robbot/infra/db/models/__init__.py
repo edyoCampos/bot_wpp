7f310c8d14854de9fe82baeb007b5d03176370d9
edyoCampos <camposevero@gmail.com>
2025-12-12 11:53:21 -0300

    refactor: refatora __init__.py

--- src/robbot/infra/db/models/__init__.py
+++ src/robbot/infra/db/models/__init__.py
@@ -5,7 +5,9 @@ from robbot.infra.db.models.message_location_model import MessageLocationModel
 from robbot.infra.db.models.message_media_model import MessageMediaModel
 from robbot.infra.db.models.message_model import MessageModel
 from robbot.infra.db.models.revoked_token_model import RevokedTokenModel
+from robbot.infra.db.models.session_model import WhatsAppSession
 from robbot.infra.db.models.user_model import UserModel
+from robbot.infra.db.models.webhook_log_model import WebhookLog
 
 __all__ = [
     "AlertModel",
@@ -14,4 +16,6 @@ __all__ = [
     "MessageLocationModel",
     "RevokedTokenModel",
     "UserModel",
+    "WhatsAppSession",
+    "WebhookLog",
 ]
