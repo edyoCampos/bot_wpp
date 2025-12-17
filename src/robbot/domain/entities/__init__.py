869b056a231009d6f7cef6c48eb6128fb5b3ba9f
edyoCampos <camposevero@gmail.com>
2025-12-16 23:13:56 -0300

    refactor: refatora __init__.py

--- src/robbot/domain/entities/__init__.py
+++ src/robbot/domain/entities/__init__.py
@@ -1,1 +1,15 @@
-__all__ = []
+"""Domain entities for the application."""
+
+from robbot.domain.entities.conversation import Conversation
+from robbot.domain.entities.conversation_message import ConversationMessage
+from robbot.domain.entities.lead import Lead
+from robbot.domain.entities.lead_interaction import LeadInteraction
+from robbot.domain.entities.llm_interaction import LLMInteraction
+
+__all__ = [
+    "Conversation",
+    "ConversationMessage",
+    "Lead",
+    "LeadInteraction",
+    "LLMInteraction",
+]
