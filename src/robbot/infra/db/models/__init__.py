a10fdec2f9d5eb4731a9ba6d6e7b4182528bd3be
edyoCampos <camposevero@gmail.com>
2025-12-16 23:13:58 -0300

    refactor: refatora __init__.py

--- src/robbot/infra/db/models/__init__.py
+++ src/robbot/infra/db/models/__init__.py
@@ -1,6 +1,13 @@
 """Database models package exports."""
 
+from robbot.domain.enums import InteractionType
 from robbot.infra.db.models.alert_model import AlertModel
+from robbot.infra.db.models.conversation_context_model import ConversationContextModel
+from robbot.infra.db.models.conversation_message_model import ConversationMessageModel
+from robbot.infra.db.models.conversation_model import ConversationModel
+from robbot.infra.db.models.lead_interaction_model import LeadInteractionModel
+from robbot.infra.db.models.lead_model import LeadModel
+from robbot.infra.db.models.llm_interaction_model import LLMInteractionModel
 from robbot.infra.db.models.message_location_model import MessageLocationModel
 from robbot.infra.db.models.message_media_model import MessageMediaModel
 from robbot.infra.db.models.message_model import MessageModel
@@ -9,12 +16,23 @@ from robbot.infra.db.models.session_model import WhatsAppSession
 from robbot.infra.db.models.user_model import UserModel
 from robbot.infra.db.models.webhook_log_model import WebhookLog
 
+# Alias for relationship references (needed by other models)
+User = UserModel
+
 __all__ = [
     "AlertModel",
+    "ConversationContextModel",
+    "ConversationMessageModel",
+    "ConversationModel",
+    "InteractionType",
+    "LeadInteractionModel",
+    "LeadModel",
+    "LLMInteractionModel",
     "MessageModel",
     "MessageMediaModel",
     "MessageLocationModel",
     "RevokedTokenModel",
+    "User",
     "UserModel",
     "WhatsAppSession",
     "WebhookLog",
