a254be17aaccd87c475713b25d257b5114c64405
edyoCampos <camposevero@gmail.com>
2025-12-26 16:20:53 -0300

    refactor: refatora __init__.py

--- src/robbot/infra/db/models/__init__.py
+++ src/robbot/infra/db/models/__init__.py
@@ -2,12 +2,12 @@
 
 from robbot.domain.enums import InteractionType
 from robbot.infra.db.models.alert_model import AlertModel
-from robbot.infra.db.models.auth_session_model import AuthSessionModel  # FASE 0
+from robbot.infra.db.models.auth_session_model import AuthSessionModel
 from robbot.infra.db.models.conversation_context_model import ConversationContextModel
 from robbot.infra.db.models.conversation_message_model import ConversationMessageModel
 from robbot.infra.db.models.conversation_model import ConversationModel
 from robbot.infra.db.models.conversation_tag_model import ConversationTagModel
-from robbot.infra.db.models.credential_model import CredentialModel  # FASE 0
+from robbot.infra.db.models.credential_model import CredentialModel
 from robbot.infra.db.models.lead_interaction_model import LeadInteractionModel
 from robbot.infra.db.models.lead_model import LeadModel
 from robbot.infra.db.models.llm_interaction_model import LLMInteractionModel
@@ -30,12 +30,12 @@ User = UserModel
 
 __all__ = [
     "AlertModel",
-    "AuthSessionModel",  # FASE 0
+    "AuthSessionModel",
     "ConversationContextModel",
     "ConversationMessageModel",
     "ConversationModel",
     "ConversationTagModel",
-    "CredentialModel",  # FASE 0
+    "CredentialModel",
     "InteractionType",
     "LeadInteractionModel",
     "LeadModel",
